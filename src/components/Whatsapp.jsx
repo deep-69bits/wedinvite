b82a1fe560ab8fb4a4ab9e3076e6352d4a579231
deep-69bits <deepakchattwaniking15@gmail.com>
2023-04-21 21:06:37 +0530

    changes

---
+++ src/components/Whatsapp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Whatsapp = () => {
+  return (
+    <div className='w-16 h-16 cursor-pointer     duration-500 transition-all hover:scale-105  rounded-full fixed z-50 bottom-10 left-8'>
+    <a href="https://wa.me/919024446964" target={"__blank"}>
+    <img  className='rounded-full  shadow-2xl shadow-pink-500 animate-bounce' src="https://cdn-icons-png.flaticon.com/512/124/124034.png" alt="" />
+    </a>
+    </div>
+  )
+}
+
+export default Whatsapp
