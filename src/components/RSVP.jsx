f14b9e3f2bddf745b0007b581d1b17375860410a
deep-69bits <deepakchattwaniking15@gmail.com>
2023-04-21 21:08:14 +0530

    changes

--- src/components/RSVP.jsx
+++ src/components/RSVP.jsx
@@ -3,10 +3,32 @@ export default function RSVP(){
     return(
         <div class="grid md:grid-cols-2 font-serif">
             <img src="./images/save the date images/rsvp.png" className="md:w-full md:h-screen w-0 h-0 object-cover mr-0"/>
-            <div className="mx-auto max-w-xl md:pt-40 md:text-8xl text-6xl text-sky-800 md:ml-[20%] ml-10 pt-10">RSVP
-                <p class="text-lg md:text-xl lg:text-2xl text-center py-4 md:py-6 lg:py-8 px-6 md:px-12 lg:px-24">
-                  I, <input type="text" placeholder="Name" class="mb-3 w-32 md:w-48 lg:w-64 border border-gray-400 rounded-sm px-2 py-1" /> am <input type="text" placeholder="Attending" class="my-3 w-32 md:w-48 lg:w-64 border border-gray-400 rounded-sm px-2 py-1" /> and I am bringing <input type="number" placeholder="No. pers" class="mt-3 w-32 md:w-48 lg:w-64 border border-gray-400 rounded-sm px-2 py-1" /> with me.
-                </p>
+            <div className=" mx-20 flex my-40 ">
+             <div>
+             <h1 className="fonthead text-7xl font-semibold my-10 text-blue-800">
+             RSVP
+             </h1>
+             <p className="text-xl">
+             I, <input type="text" className="border-[1px] text-gray-500 border-gray-400 focus:outline-none w-[200px] py-1 px-2 mx-3"   /> am 
+             <select name="" className="bg-white border-[1px] text-gray-500 border-gray-400 h-10 focus:outline-none w-[200px] py-1 px-2 mx-3" id="">
+             <option  className="border-[1px]" value="">Attending</option>
+             
+             </select>
+             <br />
+             <br />
+             and i am bringing
+             <select name="" className="bg-white border-[1px] text-gray-500 border-gray-400 h-10 focus:outline-none w-[200px] py-1 px-2 mx-3" id="">
+             <option  className="border-[1px] text-gray-500" value="">One Person</option>
+             <option  className="border-[1px] text-gray-500" value="">Two Persons</option>
+             <option  className="border-[1px] text-gray-500" value="">Three  Persons</option>
+             <option  className="border-[1px] text-gray-500" value="">Four Persons</option>
+             </select>
+             with me.
+             </p>
+               
+             <button className='w-1/2 mx-auto border-[1px] my-10 border-pink-500 bg-pink-500 text-white font-semibold text-xl text-center py-2'>Submit</button>
+             </div>
+              
             </div>
         </div>    
     )
